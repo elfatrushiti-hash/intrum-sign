0e94e112892de419720fc9255589483795c8d66b
elfatrushiti-hash <elfat.rushiti@gmail.com>
2026-03-30 13:00:19 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Offer from "./pages/Offer";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* Dashboard (bestehende App) */}
+      <Route path="/" element={<App />} />
+
+      {/* ✅ Neue Offerte-Seite */}
+      <Route path="/offer" element={<Offer />} />
+    </Routes>
+  </BrowserRouter>
 );
